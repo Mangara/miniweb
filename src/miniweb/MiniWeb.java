0a7565b8c78f0e8da924cda2535737f35f9f2d67
Sander Verdonschot <5864726+Mangara@users.noreply.github.com>
2016-01-29 18:04:22 -0500

    Source code for jsoup.

--- src/miniweb/MiniWeb.java
+++ src/miniweb/MiniWeb.java
@@ -5,6 +5,18 @@
  */
 package miniweb;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.MinifyVisitor;
+import org.jsoup.nodes.Node;
+import org.jsoup.select.NodeTraversor;
+
 /**
  *
  * @author Sander Verdonschot <sander.verdonschot at gmail.com>
@@ -14,8 +26,34 @@ public class MiniWeb {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws IOException {
+        File test = new File("testInputs/CG-Publy/CG-Lab.html");
+        Document doc = Jsoup.parse(test, "UTF-8");
+
+        System.out.println(minify(doc));
     }
 
+    private static List<Path> getExternalStyleSheets(Document doc) {
+        return doc.select("link[rel=stylesheet]").stream()
+                .map(e -> Paths.get(e.attr("href")))
+                .collect(Collectors.toList());
+    }
+
+    private static List<Path> getExternalJavascript(Document doc) {
+        return doc.select("script[src]").stream()
+                .filter(e -> !e.attr("src").startsWith("http")) // Only keep local JS files
+                .map(e -> Paths.get(e.attr("src")))
+                .collect(Collectors.toList());
+    }
+
+    private static String minify(Document doc) {
+        StringBuilder minified = new StringBuilder();
+        NodeTraversor minifyTraversor = new NodeTraversor(new MinifyVisitor(minified));
+
+        for (Node childNode : doc.childNodes()) {
+            minifyTraversor.traverse(childNode);
+        }
+
+        return minified.toString();
+    }
 }
