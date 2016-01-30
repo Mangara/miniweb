cf978002a1bb5be4674a8ce385acf32d3e4d8a47
Sander Verdonschot <5864726+Mangara@users.noreply.github.com>
2016-01-30 14:19:59 -0500

    Parsing CSS with jStyleParser, and counting HTML class occurrances.

--- src/miniweb/MiniWeb.java
+++ src/miniweb/MiniWeb.java
@@ -5,12 +5,21 @@
  */
 package miniweb;
 
-import java.io.File;
+import cz.vutbr.web.css.CSSException;
+import cz.vutbr.web.css.CSSFactory;
+import cz.vutbr.web.css.MediaSpecNone;
+import cz.vutbr.web.css.NetworkProcessor;
+import cz.vutbr.web.css.StyleSheet;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.zip.GZIPInputStream;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.MinifyVisitor;
@@ -26,11 +35,45 @@ public class MiniWeb {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws IOException {
-        File test = new File("testInputs/CG-Publy/CG-Lab.html");
-        Document doc = Jsoup.parse(test, "UTF-8");
+    public static void main(String[] args) throws IOException, CSSException {
+        Path inputDir = Paths.get("testInputs/ColorZebra");
+        Path outputDir = Paths.get("testOutputs/ColorZebra");
+        Path input = inputDir.resolve("index.html");
 
-        System.out.println(minify(doc));
+        Document doc = Jsoup.parse(input.toFile(), "UTF-8");
+
+        Map<String, Integer> htmlClassOccurrences = ClassCounter.countClasses(doc);
+
+        System.out.println("Class occurrences: " + htmlClassOccurrences);
+
+        List<Path> externalCSS = getExternalStyleSheets(doc);
+
+        for (Path cssFile : externalCSS) {
+            CSSFactory.setAutoImportMedia(new MediaSpecNone());
+            StyleSheet css = CSSFactory.parse(inputDir.resolve(cssFile).toUri().toURL(), new NetworkProcessor() {
+
+                @Override
+                public InputStream fetch(URL url) throws IOException {
+                    System.err.println("Asked to fetch " + url);
+                    
+                    if (url.toString().startsWith("file")) {
+                        return url.openConnection().getInputStream();
+                    } else {
+                        return new InputStream() {
+
+                            @Override
+                            public int read() throws IOException {
+                                return -1;
+                            }
+                        };
+                    }
+                }
+            }, "UTF-8");
+            System.out.println("CSS from " + cssFile + ":");
+            System.out.println(css);
+        }
+
+        //System.out.println(minify(doc));
     }
 
     private static List<Path> getExternalStyleSheets(Document doc) {
