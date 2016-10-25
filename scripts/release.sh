2ba17e8d97ca952a1e83736fb9e2aa23d17448d2
Sander Verdonschot <5864726+Mangara@users.noreply.github.com>
2016-10-25 00:21:52 -0400

    Small improvements.

--- scripts/release.sh
+++ scripts/release.sh
@@ -92,7 +92,7 @@ echo "done."
 
 # Verify that all tests are passing
 echo "Verifying that all unit tests are passing... "
-ant test > "$TEST_OUTPUT"
+ant test > "$TEST_OUTPUT" 2> "$TEST_OUTPUT"
 if grep -Fq "FAILED" "$TEST_OUTPUT"
 then
     echo "ERROR: Some tests failed. For details, see \"$TEST_OUTPUT\"."
@@ -105,7 +105,9 @@ echo "done."
 
 # Remove the old jar file
 echo -n "Removing old jar file... "
-rm MiniWeb*.jar
+for f in MiniWeb*.jar; do
+    [ -e "$f" ] && rm "$f"
+done
 echo "done."
 
 
